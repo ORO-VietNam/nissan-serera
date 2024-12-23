04f3e3d5608f4a90d2d6fd49751083a9b3f76e80
Thuan Lang <lang.thuam@oro.com>
2024-12-23 11:21:53 +0700

    demo

--- src/App.vue
+++ src/App.vue
@@ -1,150 +1,27 @@
 <template>
     <div class="p-5">
-        <v-stage ref="stage" :config="configKonva">
-            <v-layer :config="layerCar">
-                <v-Rect :config="car"
-                ></v-Rect>
-                <v-Rect 
-                    :config="seat1"
-                    @mouseenter="mouseMove"
-                    @mouseleave="mouseLeave"
-                    ></v-Rect>
-                    <v-Rect 
-                    :config="seat2"
-                    @mouseenter="mouseMove"
-                    @mouseleave="mouseLeave"
-                    ></v-Rect>
-                    <v-Rect :config="seat3"></v-Rect>
-                    <v-image 
-                        :config="son" 
-                        @mouseenter="mouseDrag" 
-                        @mouseleave="mouseLeave" />
-                    <v-image 
-                        :config="dad" 
-                        @mouseenter="mouseDrag" 
-                        @mouseleave="mouseLeave"/>
-                    
-            </v-layer>
-        </v-stage>
+        <div>
+            <select class="border border-black/55 h-10 w-32" name="" id="" v-model="layout">
+                <option value="1">Layout 1</option>
+                <option value="2">Layout 2</option>
+                <option value="3">Layout 3</option>
+            </select>
+        </div>
+        <div>
+            <Layout1  v-if="layout == 1"/> 
+            <Layout2  v-if="layout == 2"/> 
+            <Layout3  v-if="layout == 3"/> 
+        </div>
     </div>
 </template>
 
 <script setup>
+    import Layout1 from './layout/layout1.vue'
+    import Layout2 from './layout/layout2.vue'
+    import Layout3 from './layout/layout3.vue'
     import { ref, onMounted } from 'vue'
-    let son = ref({
-        image: null,
-        x: 700,
-        y: 0,
-        width: 60,
-        height: 60,
-        draggable: true,
-    })
+    let layout = ref(1)
 
-    let dad = ref({
-        image: null,
-        x: 700,
-        y: 80,
-        width: 60,
-        height: 60,
-        draggable: true,
-    })
-
-    let configKonva = ref({
-        width: 800,
-        height: 250,
-    })
-
-    let layerCar = ref({
-        clip:{ 
-            x: 0, 
-            y: 0, 
-            width: 600
-        }
-    })
-    let car = ref({
-        x: 0,
-        y: 0,
-        width: 600,
-        height: 250,
-        fill: 'black',
-    })
-    let seat1 = ref({
-        x: 50,
-        y: 170,
-        width: 70,
-        height: 70,
-        fill: 'green',
-        stroke: 'black',
-        strokeWidth: 0,
-        draggable: true,
-        dragBoundFunc: function (pos) {
-            // console.log(pos.x)
-            let x = pos.x < 25 ? 25 : pos.x > 75 ? 75 : pos.x;
-            return {
-                x: x,
-                y: x != 255 ? this.absolutePosition().y : pos.y,
-            };
-        },
-    })
-    let seat2 = ref({
-        x: 200,
-        y: 170,
-        width: 70,
-        height: 70,
-        fill: 'green',
-        stroke: 'black',
-        strokeWidth: 0,
-        draggable: true,
-        dragBoundFunc: function (pos) {
-            // console.log(pos.x)
-            let x = pos.x < 175 ? 175 : pos.x > 225 ? 225 : pos.x;
-            return {
-                x: x,
-                y: x != 255 ? this.absolutePosition().y : pos.y,
-            };
-        },
-    })
-    let seat3 = ref({
-        x: 350,
-        y: 170,
-        width: 70,
-        height: 70,
-        fill: 'green',
-        stroke: 'black',
-        strokeWidth: 0,
-    })
-
-    function mouseMove(e) {
-        console.log('move')
-        const stage = e.target.getStage();
-        stage.container().style.cursor = 'ew-resize';
-    }
-    
-    function mouseDrag(e) {
-        console.log('drag')
-        const stage = e.target.getStage();
-        stage.container().style.cursor = 'move';
-    }
-
-    function mouseLeave(e) {
-        console.log('leave')
-        const stage = e.target.getStage();
-        stage.container().style.cursor = 'default';
-    }
-
-    async function loadSon(img) {
-        return new Promise((resolve, reject) => {
-            const image = new Image();
-            image.src = img;
-            image.onload = () => resolve(image); 
-            image.onerror = reject; 
-        });
-    }
-
-    onMounted(async function() {
-        son.value.image = await loadSon("./src/images/son.png");
-        dad.value.image = await loadSon("./src/images/dad.png");
-    })
 </script>
 
 <style>
---
+++ src/layout/layout1.vue
@@ -0,0 +1,193 @@
+<template>
+    <div class="p-5">
+        <v-stage ref="stage" :config="configKonva">
+            <v-layer :config="layerCar">
+                <v-image 
+                    :config="car" 
+                    @mouseenter="mouseDrag" 
+                    @mouseleave="mouseLeave"/>
+                <!-- <v-Rect :config="carRec">
+                </v-Rect> -->
+                <v-Rect 
+                    :config="seat1"
+                    @mouseenter="mouseMove"
+                    @mouseleave="mouseLeave"
+                />
+                <v-Rect 
+                    :config="seat2"
+                    @mouseenter="mouseMove"
+                    @mouseleave="mouseLeave"
+                />
+                <v-Rect :config="seat3" />
+                <v-Rect 
+                    :config="vali" 
+                    @mouseenter="mouseDrag" 
+                    @mouseleave="mouseLeave"
+                />
+                <v-image 
+                        :config="son" 
+                        @mouseenter="mouseDrag" 
+                        @mouseleave="mouseLeave" />
+                <v-image 
+                        :config="dad" 
+                        @mouseenter="mouseDrag" 
+                        @mouseleave="mouseLeave"/>
+            </v-layer>
+        </v-stage>
+    </div>
+</template>
+
+<script setup>
+    import { ref, onMounted } from 'vue'
+    let son = ref({
+        image: null,
+        x: 700,
+        y: 0,
+        width: 60,
+        height: 60,
+        draggable: true,
+        dragBoundFunc: function (pos) {
+            return {
+                x: pos.x > 740 ? 740 : pos.x <= 0 ? 0 : pos.x,
+                y: pos.y > 190 ? 190 : pos.y <= 0 ? 0 : pos.y
+            }
+        }
+    })
+
+    let dad = ref({
+        image: null,
+        x: 700,
+        y: 80,
+        width: 60,
+        height: 60,
+        draggable: true,
+        dragBoundFunc: function (pos) {
+            return {
+                x: pos.x > 740 ? 740 : pos.x <= 0 ? 0 : pos.x,
+                y: pos.y > 190 ? 190 : pos.y <= 0 ? 0 : pos.y
+            }
+        }
+    })
+
+    let car = ref({
+        image: null,
+        x: 0,
+        y: 0,
+        width: 600,
+        height: 267,
+        draggable: false,
+    })
+
+    let configKonva = ref({
+        width: 800,
+        height: 250,
+    })
+
+    let layerCar = ref({
+        clip:{ 
+            x: 0, 
+            y: 0, 
+            width: 600
+        }
+    })
+    let carRec = ref({
+        x: 0,
+        y: 0,
+        width: 600,
+        height: 250,
+        fill: 'black',
+    })
+    let seat1 = ref({
+        x: 200,
+        y: 150,
+        width: 70,
+        height: 70,
+        fill: 'green',
+        stroke: 'black',
+        strokeWidth: 0,
+        draggable: true,
+        dragBoundFunc: function (pos) {
+            // console.log(pos.x)
+            let x = pos.x < 180 ? 180 : pos.x > 220 ? 220 : pos.x;
+            return {
+                x: x,
+                y: this.absolutePosition().y ,
+            };
+        },
+    })
+    let seat2 = ref({
+        x: 350,
+        y: 150,
+        width: 70,
+        height: 70,
+        fill: 'green',
+        stroke: 'black',
+        strokeWidth: 0,
+        draggable: true,
+        dragBoundFunc: function (pos) {
+            // console.log(pos.x)
+            let x = pos.x < 310 ? 310 : pos.x > 370 ? 370 : pos.x;
+            return {
+                x: x,
+                y: this.absolutePosition().y,
+            };
+        },
+    })
+    
+    let seat3 = ref({
+        x: 450,
+        y: 150,
+        width: 70,
+        height: 70,
+        fill: 'green',
+        stroke: 'black',
+        strokeWidth: 0,
+    })
+
+    let vali = ref({
+        x: 700,
+        y: 160,
+        width: 35,
+        height: 70,
+        fill: 'black',
+        stroke: 'black',
+        strokeWidth: 0,
+        draggable: true,
+        dragBoundFunc: function (pos) {
+            return {
+                x: pos.x > 765 ? 765 : pos.x <= 0 ? 0 : pos.x,
+                y: pos.y > 180 ? 180 : pos.y <= 0 ? 0 : pos.y
+            }
+        }
+    })
+
+    function mouseMove(e) {
+        const stage = e.target.getStage();
+        stage.container().style.cursor = 'ew-resize';
+    }
+    
+    function mouseDrag(e) {
+        const stage = e.target.getStage();
+        stage.container().style.cursor = 'move';
+    }
+
+    function mouseLeave(e) {
+        const stage = e.target.getStage();
+        stage.container().style.cursor = 'default';
+    }
+
+    async function loadSon(img) {
+        return new Promise((resolve, reject) => {
+            const image = new Image();
+            image.src = img;
+            image.onload = () => resolve(image); 
+            image.onerror = reject; 
+        });
+    }
+
+    onMounted(async function() {
+        son.value.image = await loadSon("./src/images/son.png");
+        dad.value.image = await loadSon("./src/images/dad.png");
+        car.value.image = await loadSon("./src/images/car.jpg");
+    })
+</script>
